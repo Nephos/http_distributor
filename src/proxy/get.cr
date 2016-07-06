60514a465f4e0e836e2c47280f78fabba761e599
Arthur Poulet <arthur.poulet@mailoo.org>
2016-07-06 22:32:27 +0200

    Improve proxy /get

--- src/proxy/get.cr
+++ src/proxy/get.cr
@@ -1,15 +1,28 @@
-get "/get/:url" do |env|
+def root_get_url(env)
   url = env.params.url["url"]?
-  if url
+  return "No url base64 encoded provided..." if url.nil?
+
+  begin
     url = Base64.decode_string(url).strip
     host = URI.parse(url).host.to_s
-    $domains[host] = HttpDistributor::Domain.new($config) unless $domains[host]?
-    $domains[host].use do
+  rescue err
+    STDERR.puts
+    env.response.status_code = 400
+    return "Invalid url \"#{url}\""
+  end
+
+  $domains[host] = HttpDistributor::Domain.new($config) unless $domains[host]?
+  $domains[host].use do
+    begin
       $client.req.get(url, $client.headers) do |res|
         res.body_io.gets_to_end
       end
+    rescue err
+      STDERR.puts "Error fetching: \"#{url}\": #{err}"
     end
-  else
-    "No url base64 encoded provided..."
   end
+end
+
+get "/get/:url" do |env|
+  root_get_url env
 end
