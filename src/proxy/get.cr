e384c0de01da2e496e8c730ee3cfd41055226da2
Arthur Poulet <arthur.poulet@mailoo.org>
2017-01-23 14:03:55 +0000

    Improve proxy code quality

--- src/proxy/get.cr
+++ src/proxy/get.cr
@@ -1,10 +1,27 @@
-private def root_get_url(env)
+private def root_get_body(url)
+  CLIENT.exec("GET", url) do |res|
+    if res.headers["Content-Encoding"]? == "gzip"
+      Zlib::Inflate.gzip(res.body_io) do |inflate|
+        inflate.gets_to_end
+      end
+    else
+      res.body_io.gets_to_end
+    end
+  end
+end
+
+private def root_extract_url(url)
+  url = Base64.decode_string(url).strip
+  host = URI.parse(url).host.to_s
+  return {url, host}
+end
+
+private def root_get(env)
   url = env.params.url["url"]?
   return "No url base64 encoded provided..." if url.nil?
 
   begin
-    url = Base64.decode_string(url).strip
-    host = URI.parse(url).host.to_s
+    url, host = root_extract_url url
   rescue err
     STDERR.puts err
     env.response.status_code = 400
@@ -14,15 +31,7 @@ private def root_get_url(env)
   DOMAINS[host] = HttpDistributor::Domain.new(CONFIG) unless DOMAINS[host]?
   DOMAINS[host].use do
     begin
-      CLIENT.exec("GET", url) do |res|
-        if res.headers["Content-Encoding"]? == "gzip"
-          Zlib::Inflate.gzip(res.body_io) do |inflate|
-            inflate.gets_to_end
-          end
-        else
-          res.body_io.gets_to_end
-        end
-      end
+      root_get_body(url)
     rescue err
       STDERR.puts "Error fetching: \"#{url}\": #{err}"
     end
@@ -30,5 +39,5 @@ private def root_get_url(env)
 end
 
 get "/get/:url" do |env|
-  root_get_url env
+  root_get env
 end
